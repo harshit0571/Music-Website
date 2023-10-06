496c831a977fae602c6a406f431d4bdcd36fd0cf
harshit0571 <harshitchadha65@gmail.com>
2023-10-06 20:01:44 +0530

    section 3 made

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import { Header } from "./Header";
 import HeaderBottom from "./HeaderBottom";
 import Section1 from "./Section1";
 import Section2 from "./Section2";
+import Section3 from "./Section3";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <HeaderBottom />
       <Section1 />
       <Section2 />
+      <Section3 />
     </div>
   );
 };
---
+++ src/Section3.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+
+const Section3 = () => {
+  return (
+    <div className="flex flex-col justify-between items-center w-[100%] md:w-[50%] gap-6 mt-6">
+      <div className="w-[300px] h-full flex flex-col rounded p-4 bg-white">
+        <div className="flex flex-col">
+          <img
+            src="https://cdn.pixabay.com/photo/2018/05/18/15/30/web-design-3411373_1280.jpg"
+            className="w-full rounded"
+          />
+          <div className="w-full text-white font-bold text-xl bg-red-800 p-2">
+            <h1>RINGTONE</h1>
+          </div>
+          <div className="bg-yellow-200 p-1 w-full flex flex-col">
+            <h1>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Obcaecati porro dolor, cumque sit sapiente
+            </h1>
+            <div className="w-full flex justify-end">
+              <button className="text-white font-bold text-xs bg-red-500 py-2 px-3 rounded-md">
+                View All
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-[300px] h-full flex flex-col rounded p-4 bg-white">
+        <div className="flex flex-col">
+          <img
+            src="https://cdn.pixabay.com/photo/2018/05/18/15/30/web-design-3411373_1280.jpg"
+            className="w-full rounded"
+          />
+          <div className="w-full text-white font-bold text-xl bg-red-800 p-2">
+            <h1>RINGTONE</h1>
+          </div>
+          <div className="bg-yellow-200 p-1 w-full flex flex-col">
+            <h1>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Obcaecati porro dolor, cumque sit sapiente
+            </h1>
+            <div className="w-full flex justify-end">
+              <button className="text-white font-bold text-xs bg-red-500 py-2 px-3 rounded-md">
+                View All
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-[300px] h-full flex flex-col rounded p-4 bg-white">
+        <div className="flex flex-col">
+          <img
+            src="https://cdn.pixabay.com/photo/2018/05/18/15/30/web-design-3411373_1280.jpg"
+            className="w-full rounded"
+          />
+          <div className="w-full text-white font-bold text-xl bg-red-800 p-2">
+            <h1>RINGTONE</h1>
+          </div>
+          <div className="bg-yellow-200 p-1 w-full flex flex-col">
+            <h1>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Obcaecati porro dolor, cumque sit sapiente
+            </h1>
+            <div className="w-full flex justify-end">
+              <button className="text-white font-bold text-xs bg-red-500 py-2 px-3 rounded-md">
+                View All
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Section3;
